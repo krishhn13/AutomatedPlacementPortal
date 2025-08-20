4d6a3e44019fd044fb89efbcc44ccf28300f9634
Krishn <chiranjiv.shah783@gmail.com>
2025-08-20 14:38:56 +0530

    Set up the backend structure

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+})
+.then(() => {
+        console.log("Connected to MongoDB");
+})
+.catch((err) => {
+        console.error("Failed to connect to MongoDB:", err);
+});
+
+app.listen(PORT, () => {
+        console.log(`Server is successfully established on http://localhost:${PORT}`);
+});
