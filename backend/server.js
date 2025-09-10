6086e2247a96a04df8a018319327e94656ba89b0
AbhinavGupta-012 <abhinavgupta1702@gmail.com>
2025-09-10 14:26:15 +0530

    Changed index.js to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,18 @@ require('dotenv').config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+
 const app = express();
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
-
 
 // Connect to MongoDB
 mongoose.connect(MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-})
-.then(() => {
+}).then(() => {
         console.log("Connected to MongoDB");
-})
-.catch((err) => {
+}).catch((err) => {
         console.error("Failed to connect to MongoDB:", err);
 });
 
