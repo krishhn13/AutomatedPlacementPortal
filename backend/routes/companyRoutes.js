954728ef9735a966fd825982dbb80245814e1088
Krishn <chiranjiv.shah783@gmail.com>
2025-09-15 20:38:24 +0530

    Added multiple routes

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const companyController = require("../controllers/companyController")
 
 router.get("/companies",companyController.getAll);
+router.get("/company/:name",companyController.getByName());
 router.post("/addCompany",companyController.addCompany);
-
+router.put("/updateCompany",companyController.updateCompany);
+router.delete("/deleteCompany",companyController.deleteCompany);
 
 
 
