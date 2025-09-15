5efc25699ffde410d0fcebbd2dc14f2e873e0f1b
Krishn <chiranjiv.shah783@gmail.com>
2025-09-15 20:03:43 +0530

    Updated routes

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -2,4 +2,14 @@ const express = require('express')
 const router = express.Router()
 const companyController = require("../controllers/companyController")
 
-router.get("/companies",companyController.getAll());
+router.get("/companies",companyController.getAll);
+router.post("/addCompany",companyController.addCompany);
+
+
+
+
+
+
+
+
+module.exports = router;
