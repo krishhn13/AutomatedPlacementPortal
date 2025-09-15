37dcd973f67f1d695353422b3ae876f9901f0a2b
Krishn <chiranjiv.shah783@gmail.com>
2025-09-15 19:54:14 +0530

    Updated company controller

--- backend/controllers/companyController.js
+++ backend/controllers/companyController.js
@@ -1,2 +1,9 @@
-const express = require('express')
 const companyModel = require("../models/CompanyModel")
+
+const getAll = async (req,res) => {
+        return companyModel.find();
+}
+
+module.exports = {
+        getAll
+}
