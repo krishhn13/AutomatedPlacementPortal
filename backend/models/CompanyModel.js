979de20564cb57e15dc452675f9e38b353af401f
Krishn <chiranjiv.shah783@gmail.com>
2025-09-16 15:10:13 +0530

    Made company name mandatory

--- backend/models/CompanyModel.js
+++ backend/models/CompanyModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const CompanySchema = new mongoose.Schema({
         name: { 
                 type: String,
-                // required: true
+                required: true
         },
         location: { 
                 type: String,
