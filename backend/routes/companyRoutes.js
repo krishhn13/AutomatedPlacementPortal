913df63fb3add0e8e41e66ce3171b25f35bd3613
Krishn <chiranjiv.shah783@gmail.com>
2025-09-17 21:40:22 +0530

    Updated dependencies

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express')
-const router = express.Router()
 const companyController = require("../controllers/companyController")
+const router = express.Router()
 
 router.get("/companies",companyController.getAll);
 router.get("/company/:name",companyController.getByName);
 router.post("/addCompany",companyController.addCompany);
 router.put("/updateCompany/:name",companyController.updateCompany);
 router.delete("/deleteCompany/:name",companyController.deleteCompany);
-
 
 
 module.exports = router;
