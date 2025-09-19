cdc983afcc69f1f016d6b7f97f6cfe0c4a8e683e
Krishn <chiranjiv.shah783@gmail.com>
2025-09-19 14:37:51 +0530

    Updated routes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const controller = require('../controllers/adminController')
 
-
+router.get('/students',controller.getStudents);
 
 
 
