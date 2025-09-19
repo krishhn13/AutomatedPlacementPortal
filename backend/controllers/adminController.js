74a9262ba106f85488894b1ca877993abf780cc6
Krishn <chiranjiv.shah783@gmail.com>
2025-09-19 14:49:14 +0530

    Updated adminController

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const model = 
+const Students = require("../models/StudentModel")
 
 const getStudents = async(req , res) => {
-
+        try {
+                res.status(200).json({
+                        data : Students.find()
+                })
+        } catch (err) {
+                res.status(500).json({
+                        message : "Sorry for the inconvenience, server failed abruptly"
+                })
+        }
 }
