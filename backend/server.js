f2c41be54f7b8228f756de0a6fc611b2344be844
AbhinavGupta-012 <abhinavgupta1702@gmail.com>
2025-09-21 17:13:14 +0530

    updated server.js and package.json

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,32 @@
-// requiring essential modules
 require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
-const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
-
-
-// Connect to MongoDB
-mongoose.connect(MONGODB_URI)
-.then(() => {
-        console.log("Connected to MongoDB");
-}).catch((err) => {
-        console.error("Failed to connect to MongoDB:", err);
-});
-
-
-
-
-// middlewares
 const morgan = require('morgan');
 const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
 app.use(morgan('tiny'));
 app.use(cors());
-
-
-
-
-
-// importing the files
-const company = require("./routes/companyRoutes")
-
-
-
-
-
-
-//using the routes
-app.use(express.json());  
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api',company); 
 
+const companyRoutes = require("./routes/companyRoutes");
+const studentRoutes = require("./routes/studentRoutes");
+const authRoutes = require("./routes/authRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
+app.use('/api', authRoutes);
+app.use('/api', studentRoutes);
+app.use('/api', companyRoutes);
+app.use('/api', adminRoutes);
 
 app.listen(PORT, () => {
-        console.log(`Server is successfully established on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
