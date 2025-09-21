4164ff3c5fb4c41b36df7baf206ece2c127e6a4a
AbhinavGupta-012 <abhinavgupta1702@gmail.com>
2025-09-21 17:09:44 +0530

    updated adminController.js

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,20 +1,47 @@
-const express = require('express');
-const Students = require("../models/StudentModel")
+const Students = require("../models/Student");
 
-const getStudents = async(req , res) => {
-        try {
-                res.status(200).json({
-                        data : Students.find()
-                })
-        } catch (err) {
-                res.status(500).json({
-                        message : "Sorry for the inconvenience, server failed abruptly"
-                })
-        }
-}
+const getPlacementReports = async (req, res) => {
+    try {
+        const totalStudents = await Students.countDocuments();
 
+        const placed = await Students.countDocuments({
+            status: { $exists: true, $ne: {} },
+            $expr: { $gt: [{ $size: { $filter: { input: { $objectToArray: "$status" }, cond: { $eq: ["$$this.v", "Selected"] } } } }, 0] }
+        });
 
+        const branchStats = await Students.aggregate([
+            {
+                $group: {
+                    _id: "$branch",
+                    total: { $sum: 1 },
+                    placed: {
+                        $sum: {
+                            $size: {
+                                $filter: {
+                                    input: { $objectToArray: "$status" },
+                                    cond: { $eq: ["$$this.v", "Selected"] }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        ]);
+
+        res.status(200).json({
+            totalStudents,
+            placed,
+            branchStats
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: "Server failed while generating reports"
+        });
+    }
+};
 
 module.exports = {
-        getStudents
-}
+    getPlacementReports
+};
