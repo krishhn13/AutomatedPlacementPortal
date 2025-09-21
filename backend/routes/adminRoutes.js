d9f8f65fcac5c7cb2fb967e6b07c44df1f1aefdf
AbhinavGupta-012 <abhinavgupta1702@gmail.com>
2025-09-21 17:11:51 +0530

    upadted adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/adminController')
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/adminController');
+const authMiddleware = require('../middlewares/authMiddleware');
+const roleMiddleware = require('../middlewares/roleMiddleware');
 
-router.get('/students',controller.getStudents);
-
-
-
+router.get(
+    '/admin/reports',
+    authMiddleware,
+    roleMiddleware(['admin']),
+    adminController.getPlacementReports
+);
 
 module.exports = router;
