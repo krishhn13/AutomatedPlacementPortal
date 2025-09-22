b485398fbfd3c274a938eae3a29dc9fd7bb6d824
Krishn <chiranjiv.shah783@gmail.com>
2025-09-22 20:44:25 +0530

    Added Job Model

---
+++ backend/models/Job.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const Job = new mongoose.Schema({
+        title : {
+                type : String,
+                required : true
+        },
+        
+})
