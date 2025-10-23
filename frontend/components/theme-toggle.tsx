0f12729948b21af29eaee8f2099b6f232a31c8d9
Krishan <chiranjiv.shah783@gmail.com>
2025-10-23 21:36:09 +0530

    Update

--- frontend/components/theme-toggle.tsx
+++ frontend/components/theme-toggle.tsx
@@ -32,7 +32,7 @@ export function ThemeToggle() {
       <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">
         Toggle theme
-        </span>
+      </span>
     </Button>
   )
 }
