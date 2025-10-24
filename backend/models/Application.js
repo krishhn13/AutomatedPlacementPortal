a7e0a712c0dcb0093ecc1ad1f57f3dbc1cd2583a
Krishan <chiranjiv.shah783@gmail.com>
2025-10-24 20:33:08 +0530

    Added Application schema

---
+++ backend/models/Application.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const applicationSchema = new mongoose.Schema({
+  job: { type: mongoose.Schema.Types.ObjectId, ref: 'Job' },
+  student: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  appliedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Application', applicationSchema);
